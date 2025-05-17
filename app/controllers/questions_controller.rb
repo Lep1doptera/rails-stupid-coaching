2d051f22d188422ca596dd59d3b490aa475d4cdd
Amanda Ferguson <ferguson.am19@gmail.com>
2025-05-17 21:27:56 +1000

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @ask = params[:question]
+    @coach =
+      if @ask == "I am going to work"
+        "Great!"
+      elsif @ask.include? "?"
+        "Silly question, get dressed and go to work!"
+      else
+        "I don't care, get dressed and go to work!"
+      end
+
+  end
+end
